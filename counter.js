16c8047213f4e53f1ca6ad5d761005cf7b5e7e77
WesamKhalil <66225664+WesamKhalil@users.noreply.github.com>
2021-04-30 09:28:32 +0100

    Added comma rendering to numbers, tried imrpoving the explanation

--- counter.js
+++ counter.js
@@ -1,64 +1,58 @@
 // This is a function that animates counting numbers.
-// The bare minimum to get this script working is passing in a nums array with an id of where you want to animate the numbers
-// And the end which is the number you want to reach with the counting animation.
-// There are optional arguments like onShowId which is the element you want the client to see before running the script, by default it's targeting the body element.
+// The bare minimum to get this script working is passing in an array with objects that have an 'id' and 'start' property, with the 'end' property being optional.
+// If you animation is somewhere at the bottom of the page you don't want it to run until the client has scrolled down to be able to see it.
+// There are optional arguments like onShowId which is the id of the element you want the client to see before running the script, by default it's targeting the body element.
 // onShowDisplacement how far into the element you want the client to scroll before running the script.
-// nums which is an array or objects with an element id, start number and end number.
-// duration which specifies how long you want the animatin to play in seconds.
-// variety which specifies how much variety in seconds you want the animationto play compared to the core duration.
-// Here's an example of calling the function.
+// duration which specifies how long you want the animation to play in seconds.
+// variation which specifies how much variation in seconds you want the animationto play compared to the core duration.
+// Here's an example of calling the function under a C# .NET Core context.
 
 {/* 
+@{
+    var numbersList = numbers.ToList();
+}
 <script src="counter.js"></script>
 <script>
+    let numbersArray = [];
+    @for(int i = 0; i < numbersList.Count(); i++)
+    {
+        const obj@i = {
+            id: "@numbersList[i].Id",
+            start: 0,
+            end: @numbersList[i].Number
+        }
+
+        numbersArray.push(obj@i);
+    }
+
     counterAnimation({
-        onShowId: "#nums",
+        onShowId: "#numbersContainer",
         onShowDisplacement: 200,
-        nums: [{ id: 'one', end: 10 }, { id: 'two', end: 100 }, { id: 'three', end: 1000 }, { id: 'four', end: 60 }, { id: 'five', end: 300 }, { id: 'six', end: 237 }],
+        nums: numbersArray,
         duration: 1.5,
-        variety: 1.5
+        variation: 1.5
     })
 </script> 
 */}
 
-// To get a populated nums array to use with counterAnimation write something like this.
-{/* 
-@{
-    var numList = numbers.ToList();
-}
-<script>
-    let nums = [];
-    @for(int i = 0; i < numList.Count(); i++)
-    {
-        const obj@i = {
-            id: "@numList[i].Id",
-            start: 0,
-            end: @numList[i].End
-        }
-
-        nums.push(obj@i);
-    }
-</script> 
-*/}
-
-const counterAnimation = ({ onShowId = "body", onShowDisplacement = 0, nums = [], duration = 1, variety = 1 }) => {
+const counterAnimation = ({ onShowId = "body", onShowDisplacement = 0, nums = [], duration = 1, variation = 1 }) => {
 
     const runAnimation = ({ id = "#", end = 100, start = 0 }) => {
         if (start === end) return;
         const range = end - start;
         let current = start;
-        const increment = (end > start ? 1 : -1) * range / 60 * (duration + Math.random() * variety);
+        const increment = (end > start ? 1 : -1) * range / (duration + Math.random() * variation) / 60;
         const obj = document.getElementById(id);
         const timer = setInterval(() => {
             current += increment;
 
             if (current >= end) {
                 clearInterval(timer);
-                obj.innerHTML = end;
+                obj.innerHTML = end.toLocaleString();
                 return;
             }
 
-            obj.innerHTML = Math.round(current);
+            obj.innerHTML = Math.round(current).toLocaleString();
         }, 1000 / 60);
     }
 
